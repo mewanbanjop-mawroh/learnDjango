59625f43351bb5c74c63073942df1ab63c49705f
mewanbanjop-mawroh <mewanbanjop.mawroh@gmail.com>
2016-01-31 14:27:51 -0500

    Mewan - adding basic index.html page

--- webapps/todo/urls.py
+++ webapps/todo/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^add-todo-item$', todo.views.add_todo_item, name='add_todo_item'),
-    url(r'^delete-todo-item/(?P<item_id>\d+)$', todo.views.delete_todo_item, name='delete_todo_item'),
+    url(r'^index$', views.index, name='index'),
+    url(r'^add-todo-item$', views.add_todo_item, name='add_todo_item'),
+    url(r'^delete-todo-item/(?P<item_id>\d+)$', views.delete_todo_item, name='delete_todo_item'),
 ]
--- webapps/todo/views.py
+++ webapps/todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+	return render(request, 'index.html',{})
+
+def add_todo_item(request):
+	return render(request, 'index.html',{})
+
+def delete_todo_item(request,item_id):
+	return render(request, 'index.html',{})
